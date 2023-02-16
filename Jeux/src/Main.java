2bb5a530d4944bb68f5269884e750614b06543d4
GavB0ss <gavrilou.bosso@gmail.com>
2023-02-16 08:42:07 +0100

    Hero main

--- Jeux/src/Main.java
+++ Jeux/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        
+        Hero hero = new Hero(1, 1, 0, 20, 20, 100, 10, null, 0, 0, 0, 0)
         Map map = new Map(20, 20);
         //map.showmap(map.getLatitude(),map.getLongitude());
     }
