de4ed76769d16afa490c6953ecfa44ccc47d436b
GavB0ss <gavrilou.bosso@gmail.com>
2023-02-16 11:52:28 +0100

    Map update

--- Jeux/src/Main.java
+++ Jeux/src/Main.java
@@ -1,7 +1,15 @@
 public class Main {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        int[][] mur = {{0,0},{1,1}};
+        //afficher la map apres qui a saisie son nom ... (debut de game)
+        int[][] mur = 
+        {
+            {0,0},{0,1},{0,2},{0,3},{0,4},{0,5},{0,6},{0,7},{0,8},{0,9},//haut
+            {1,0},{2,0},{3,0},{4,0},{5,0},{6,0},{7,0},{8,0},{9,0},// droite
+            {9,1},{9,2},{9,3},{9,4},{9,5},{9,6},{9,7},{9,8},{9,9}, //bas
+            {1,9},{2,9},{3,9},{4,9},{5,9},{6,9},{7,9},{8,9}, //gauche
+            {4,1},{2,3},{2,4},{3,3},{3,4},{6,2},{6,3},{6,4},{1,7},{2,7},{3,7},{7,7},{8,7},{8,8},{8,3}//mur dans la map (latitude, longitude)
+        };
 
         //Hero hero = new Hero(1, 1, 0, 20, 20, 100, 10, null, 0, 0, 0, 0)
         Map map = new Map(10, 10, mur);
--- Jeux/src/Map.java
+++ Jeux/src/Map.java
@@ -1,15 +1,15 @@
-import javax.swing.text.Position;
-
 public class Map { //class perso pour les déplacement
 
 private int latitude;
 private int longitude;
 private int[][] mur;  //matrice (définie) si > 20 mur et < 1 mur pour x et y
+private int[][] Hero; 
    
-   public Map(int latitude, int longitude, int[][] mur) {
+   public Map(int latitude, int longitude, int[][] mur, int[][] Hero) {
         this.latitude = latitude;
         this.longitude = longitude;
         this.mur = mur;
+        this.Hero = Hero;
     }
    
     public int getLatitude() {
@@ -27,31 +27,41 @@ private int[][] mur;  //matrice (définie) si > 20 mur et < 1 mur pour x et y
     public void setLongitude(int longitude) {
         this.longitude = longitude;
     }
-
-    /*public int getmur(){
-        return [][] mur;
-    }
-
-    public void setmur(int [][] mur){
-        this.mur = mur;
-    }*/
     
     public void showmap(){ //il faut parcourir la carte(pas besoin de l'afficher) (savoir ou est le personnage sur la map)
         for (int i = 0; i < latitude; i++) {
-            for (int j = 0; j < longitude; j++) { //faire en sorte de mettre voir les cordonée et faire  (detecter les cordonées)
-                if()
-                System.out.print("0\t");
+            for (int j = 0; j < longitude; j++) { //faire en sorte de mettre voir les cordonée et faire  (detecter les cordonées) si la longitude et latitude sont au possition du mur
+                
+                if(this.estMur(i, j)) {
+                    System.out.print("X\t");
+                } else {
+                    System.out.print("0\t");
+                }
                 
             }          
             System.out.println();
         }
 
-        for (int i = 0; i < mur.length; i++) {
-            for (int j = 0; j < mur.length; j++) {
-                System.out.print("X\t");
-            }
+    }
 
+    private boolean estMur(int indexLat, int indexLong) {
+        for(int i = 0; i < mur.length; i++) {
+            if(indexLat == mur[i][0] && indexLong == mur[i][1]) {
+                return true;
+            }
         }
+
+        return false;
+    }
+
+    private boolean estHero(int indexLat, int indexLong) {
+        for(int i = 0; i < Hero.length; i++) {
+            if(indexLat == Hero[i][0] && indexLong == Hero[i][1]) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
     
