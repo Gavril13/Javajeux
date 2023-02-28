f299e5a8df8679236434616281c2255825daed0f
GavB0ss <gavrilou.bosso@gmail.com>
2023-02-28 17:00:50 +0100

    Interaction Update

--- Jeux/src/Interaction.java
+++ Jeux/src/Interaction.java
@@ -1,23 +1,20 @@
 import java.util.Scanner;
 
 public class Interaction {
-    private String name;
-
-    public void start(String startgame , Hero hero1){ 
+    public void start(Scanner in, Hero hero1){ 
         System.out.println("\n=================================================================\n"+ 
         "| Es-tu prête à ce jeux ?                                       |\n"+ 
         "| 1 : Oui                                                       |\n"+ 
         "| 2 : Non                                                       |\n"+ 
         "=================================================================\n");
+        String startgame = in.next();
         switch (startgame){
-            case "Oui":
-                Scanner Choix1 = new Scanner (System.in); 
+            case "Oui": 
                 System.out.println("-----------------------------------------------------------------\n"+
                 "La Partie va commencer !\n"+
                 "Ton héro s'appelera comment ? \n"+
                 "-----------------------------------------------------------------\n");
-                String name = Choix1.next();
-                Choix1.close();
+                String name = in.next();
                 hero1.setname(name);
                 System.out.println(" \n"+
                 "Ton Nom est "+ hero1.getname() + " ! La Partie va pouvoir commencer ! \n"); 
--- Jeux/src/Main.java
+++ Jeux/src/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main { //essayer de faire juste déplacer le point H de 1 de cordonée (ou je le fais ??)
     public static void main(String[] args) throws Exception {
-        System.out.println("Jeux Java");
+        System.out.println("-------------" + " Gavril Jeux-Java " + "-------------");
         //afficher la map apres qui a saisie son nom ... (debut de game)
         int[][] mur = 
         {
@@ -26,10 +26,11 @@ public class Main { //essayer de faire juste déplacer le point H de 1 de cordon
             {9,0},{9,1},{9,2},{9,3},{9,4},{9,5},{9,6},{9,7},{9,8},{9,9}
         };
 
-        //Interaction interaction = new Interaction();
-        //interaction.start(null, null);
-
         Hero hero1 = new Hero(1, 1, 15,15, 100, 8, 20);
+
+        Scanner in = new Scanner(System.in);
+        Interaction interaction = new Interaction();
+        interaction.start(in, hero1);
 
         Monstre monstre1 = new Monstre(10, 10, 100, 5, 20, 5, 1);
 
@@ -39,11 +40,11 @@ public class Main { //essayer de faire juste déplacer le point H de 1 de cordon
 
         Monstre monstre4 = new Monstre(10, 10, 100, 5, 20, 8, 5);
 
-        Coffre coffre1 = new Coffre(0, 0, 0, 0, 1, 3);
+        Coffre coffre1 = new Coffre(0, 0, 0, 1, 3);
 
-        Coffre coffre2 = new Coffre(0, 0, 0, 0, 8, 1);
+        Coffre coffre2 = new Coffre(0, 0, 0, 8, 1);
 
-        Coffre coffre3 = new Coffre(0, 0, 0, 0, 8, 7);
+        Coffre coffre3 = new Coffre(0, 0, 0, 8, 7);
 
         Boss boss1 = new Boss(40, 50, 400, 20, 200, 2, 8);
             
@@ -62,14 +63,13 @@ public class Main { //essayer de faire juste déplacer le point H de 1 de cordon
             map.showmap();
 
             System.out.println("Vous voulez aller ou?\n(haut, bas, gauche, droite)");
-            Scanner move = new Scanner(System.in);
-            String choixdeplacement = move.nextLine();
+            String choixdeplacement = in.nextLine();
             System.out.println("vous avez choisie : " + choixdeplacement); 
 
             hero1.deplacer(choixdeplacement);
 
             Combat combat = new Combat();
-            combat.combat(hero1, monstre1, monstre2, monstre3, monstre4, boss1);
+            combat.combat(hero1, monstre1, monstre2, monstre3, monstre4, boss1, in);
 
         }
     }
